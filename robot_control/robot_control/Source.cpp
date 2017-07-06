bc66cea4838b1fd32cd40b7e5844093a4db36877
grzes_laptop <grzegorzskorupko@gmail.com>
2017-07-06 16:54:46 +0200

    basic_code

---
+++ robot_control/robot_control/Source.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+extern "C" {
+#include "gopigo.h"
+}
+
+
+using namespace std;
+
+int main(void) {
+
+	if (init() == -1) {
+		exit(1);
+	}
+
+	int komenda = 0;
+	int i = 0;
+	int dst = 0;
+
+	while (1) {
+		i++;
+		if (i > 4000)
+		{
+			komenda = 0;
+		}
+		komenda = (i > 1000 && i <3999) ? 1 : 0;
+
+		switch (komenda)
+		{
+		case 1:
+			printf("jade!");
+			fwd();
+			break;
+
+		case 0:
+			stop();
+			break;
+
+		}
+		printf("i= %d komenda= %d \n", i, komenda);
+		printf("dystans: %d\n", dst);
+		komenda = (dst > 40) ? 1 : 0;
+	}
+
+	getchar();
+	return 0;
+}
