1a7fea944e310b6110017f7e3f9039ad130de842
grzes_laptop <grzegorzskorupko@gmail.com>
2017-07-07 14:22:06 +0200

    two camera setup

--- robot_control/camera_robot/Source.cpp
+++ robot_control/camera_robot/Source.cpp
@@ -10,17 +10,25 @@ using namespace std;
 int main (void) {
 
 VideoCapture cap(0);
+VideoCapture cap2(1);
+
 Mat frame;
+Mat frame2;
 bool success;
 
 cap.set(CV_CAP_PROP_FRAME_HEIGHT, 360);
 cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-//cap2.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
-//cap2.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
+cap2.set(CV_CAP_PROP_FRAME_HEIGHT, 360);
+cap2.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 
 
 if (!cap.isOpened()) {
 	cout << "nie moge otworzyc kamery 1" << endl;
+	return -1;
+}
+
+if (!cap2.isOpened()) {
+	cout << "nie moge otworzyc kamery 2" << endl;
 	return -1;
 }
 
@@ -29,12 +37,16 @@ if (!cap.isOpened()) {
 while (true) {
 
 	cap.set(CV_CAP_PROP_EXPOSURE, -11);
+	cap2.set(CV_CAP_PROP_EXPOSURE, -11);
 
 	success = cap.grab();
+	success = cap2.grab();
 	cap.retrieve(frame);
+	cap2.retrieve(frame);
 
 	//resize(frame, frame, Size(), 0.4, 0.4, INTER_AREA);
-	imshow("kamera", frame);
+	imshow("kamera 1", frame);
+	imshow("kamera 2", frame2);
 
 	int iKey = waitKey(50);
 	if (iKey == 27)
