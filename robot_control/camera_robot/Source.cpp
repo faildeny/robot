15d21039d81f31879478264f5ef6c886d5e57aea
grzes_laptop <grzegorzskorupko@gmail.com>
2017-07-07 17:19:17 +0200

    synthesized camera with motors

--- robot_control/camera_robot/Source.cpp
+++ robot_control/camera_robot/Source.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <stdio.h>
+extern "C" {
+#include "gopigo.h"
+}
+
 
 using namespace cv;
 using namespace std;
@@ -75,6 +80,21 @@ int numberOfDisparities = 64;
 Rect roi1(1, 1, 1000, 1000);
 Rect roi2(1, 1, roiw2, roiw2);
 
+//Initialize robot
+
+if (init() == -1) {
+	exit(1);
+}
+
+char cmd[2];
+cmd[0] = 'w';
+cmd[1] = 'x';
+set_speed(30);
+
+int i = 0;
+int dst = 0;
+
+///////
 
 while (true) {
 
@@ -130,6 +150,61 @@ while (true) {
 
 	applyColorMap(preview, preview, COLORMAP_JET);
 	imshow("disparity", preview);
+
+	////////////////////////////////////
+
+	i++;
+	if (i > 100)
+	{
+
+		cmd[0] = 'x';
+		stop();
+		led_off(0);
+		led_off(1);
+		exit(0);
+		break;
+	}
+
+	switch (cmd[0])
+	{
+	case 'w':
+
+		printf("mozna jechac: ");
+		switch (cmd[1])
+		{
+		case 'w':
+			printf("jade");
+			led_off(0);
+			led_off(1);
+			//motor1(1, 30);
+			//motor2
+
+			fwd();
+			break;
+		case 'd':
+			printf("skrecam");
+			led_on(1);
+			led_on(0);
+			right_rot();
+
+
+			break;
+		}
+
+		break;
+
+	case 'x':
+		stop();
+		break;
+
+	}
+	dst = us_dist(15);
+	cmd[1] = (dst > 40) ? 'w' : 'd';
+	cmd[1] = (max < 230) ? 'w' : 'd';
+	printf("i= %d command= %c %c \n", i, cmd[0], cmd[1]);
+	printf("dystans: %d\n", dst);
+
+	////////////////////////////////
 
 	int iKey = waitKey(50);
 	if (iKey == 27)
