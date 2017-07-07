2da95b7894eff6271e34517b6d9aa898008a8b90
grzes_laptop <grzegorzskorupko@gmail.com>
2017-07-07 16:53:24 +0200

    string stream

--- robot_control/camera_robot/Source.cpp
+++ robot_control/camera_robot/Source.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace cv;
 using namespace std;
@@ -117,12 +118,14 @@ while (true) {
 	resize(disp8, disp8, Size(), 3, 3, INTER_LINEAR);
 	Mat preview;
 	double *min, *max;
+	ostringstream ss;
 	disp8.convertTo(preview, -1, double(ratio) / 50., offset - 200);
 
 	Size rozmiar = preview.size();
 	Range area(rozmiar.height/2-rozmiar.height / 4, rozmiar.height/2+rozmiar.height/4);
 	minMaxLoc(preview(area, area),min,max);
-	String text = to_string(*max);
+	ss << *max;
+	String text = ss.str();
 	putText(preview, text, punkt, CV_FONT_HERSHEY_COMPLEX, 1, Scalar(255, 250, 255), 2, CV_AA, 0);
 
 	applyColorMap(preview, preview, COLORMAP_JET);
