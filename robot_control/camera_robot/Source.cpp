2b37178a87c22dcd8f58d0b97432d77bb3e1410b
grzes_laptop <grzegorzskorupko@gmail.com>
2017-07-11 15:17:12 +0200

    another  include change

--- robot_control/camera_robot/Source.cpp
+++ robot_control/camera_robot/Source.cpp
@@ -1,7 +1,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/objdetect.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
 
 #include <iostream>
 #include <string>
