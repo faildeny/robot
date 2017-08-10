ee4b8a24aea5ee7edb3ab412b54e7aea8191f461
grzes_laptop <grzegorzskorupko@gmail.com>
2017-08-10 14:23:23 +0200

    not reading encoders

--- robot_control/camera_robot/Source.cpp
+++ robot_control/camera_robot/Source.cpp
@@ -352,7 +352,7 @@ while (true) {
 	robot.showStatus();
 
 // Odometry
-	robot.readEncoders(enc_left, enc_right);
+	//robot.readEncoders(enc_left, enc_right);
 	decodeEncoders();
 	updateCoordinates(enc_diff_left, enc_diff_right);
 	updateMap(position);
