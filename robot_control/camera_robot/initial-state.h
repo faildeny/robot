33c4754ee298ca0b886d4c027e46f61b8016bf3c
grzes_laptop <grzegorzskorupko@gmail.com>
2017-08-15 13:49:23 +0200

    include change curl

--- robot_control/camera_robot/initial-state.h
+++ robot_control/camera_robot/initial-state.h
@@ -1,4 +1,4 @@
-#include <curl\curl.h>
+#include <curl/curl.h>
 
 int create_bucket(char *access_key, char *bucket_key, char *bucket_name);
 int stream_event(char *access_key, char *bucket_key, char *json);
