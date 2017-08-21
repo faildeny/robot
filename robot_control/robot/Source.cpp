532f940ef07eab03232757815ac3e860ea6d6c88
grzes_laptop <grzegorzskorupko@gmail.com>
2017-08-21 12:58:48 +0200

    scale tweaks

--- robot_control/robot/Source.cpp
+++ robot_control/robot/Source.cpp
@@ -170,11 +170,11 @@ void map3d(Mat &map, Mat image3d) {
 			/*int x = int(5 * image3d.at<Vec3f>(j, i)[0]) + center_x);
 			int y = int(-5 * image3d.at<Vec3f>(j, i)[2]) + center_y;*/
 
-			int x1 = (1 * image3d.at<Vec3f>(j, i)[0]);
-			int y1 = int(1 * image3d.at<Vec3f>(j, i)[2]);
+			int x1 = (-0.2 * image3d.at<Vec3f>(j, i)[0]);
+			int y1 = int(-0.2 * image3d.at<Vec3f>(j, i)[2]);
 
-			int x2 = x1*(-1 - azimuth / cos(atan(y1 / x1)))+position.x+center_x;
-			int y2 = y1*(-1 - azimuth / sin(atan(y1 / x1)))+position.y+center_y;
+			int x2 = x1*(1 - azimuth / cos(atan(y1 / x1)))+position.x+center_x;
+			int y2 = y1*(1 - azimuth / sin(atan(y1 / x1)))+position.y+center_y;
 			circle(map, Point(x2, y2), 1, CV_RGB(255, 0, 0), 2);
 		}
 	}
