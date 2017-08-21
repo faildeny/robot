6c44f84566f78d8285ee6feaa8649ea45e242456
grzes_laptop <grzegorzskorupko@gmail.com>
2017-08-21 16:25:39 +0200

    highres with rotation

--- robot_control/robot/Source.cpp
+++ robot_control/robot/Source.cpp
@@ -174,10 +174,10 @@ void map3d(Mat &map, Mat image3d) {
 			int x1 = ((-5 * image3d.at<Vec3f>(j, i)[0]));
 			int y1 = int(-5 * image3d.at<Vec3f>(j, i)[2]);
 
-			//int x2 = x1/cos(atan(y1/x1))*cos(atan(y1/x1)+azimuth-0.5)+position.x+center_x;
-			//int y2 = y1 / sin(atan(y1 / x1))*sin(atan(y1 / x1) + azimuth-0.5) +position.y+center_y;
-			int x2 = x1 +position.x+ center_x;
-			int y2 = y1 + position.y + center_y;
+			int x2 = x1/cos(atan(y1/x1))*cos(atan(y1/x1)+azimuth)+position.x+center_x;
+			int y2 = y1 / sin(atan(y1 / x1))*sin(atan(y1 / x1) + azimuth) +position.y+center_y;
+			//int x2 = x1 +position.x+ center_x;
+			//int y2 = y1 + position.y + center_y;
 			circle(map, Point(x2, y2), 1, CV_RGB(255, 0, 0), 2);
 		}
 	}
