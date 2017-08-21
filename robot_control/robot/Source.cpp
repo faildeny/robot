de852c9a8271276a5626859f44fe30b253f43a38
grzes_laptop <grzegorzskorupko@gmail.com>
2017-08-21 10:40:21 +0200

    scale tweaks

--- robot_control/robot/Source.cpp
+++ robot_control/robot/Source.cpp
@@ -170,8 +170,8 @@ void map3d(Mat &map, Mat image3d) {
 			/*int x = int(5 * image3d.at<Vec3f>(j, i)[0]) + center_x);
 			int y = int(-5 * image3d.at<Vec3f>(j, i)[2]) + center_y;*/
 
-			int x1 = (5 * image3d.at<Vec3f>(j, i)[0]);
-			int y1 = int(-5 * image3d.at<Vec3f>(j, i)[2]);
+			int x1 = (1 * image3d.at<Vec3f>(j, i)[0]);
+			int y1 = int(1 * image3d.at<Vec3f>(j, i)[2]);
 
 			int x2 = x1*(1 - azimuth / cos(atan(y1 / x1)))+position.x+center_x;
 			int y2 = y1*(1 - azimuth / sin(atan(y1 / x1)))+position.y+center_y;
