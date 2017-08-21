c74c24f1b0f3ab142d3d7168cb7b03e5f80a840b
grzes_laptop <grzegorzskorupko@gmail.com>
2017-08-21 14:53:26 +0200

    diff scale

--- robot_control/robot/Source.cpp
+++ robot_control/robot/Source.cpp
@@ -171,8 +171,8 @@ void map3d(Mat &map, Mat image3d) {
 			/*int x = int(5 * image3d.at<Vec3f>(j, i)[0]) + center_x);
 			int y = int(-5 * image3d.at<Vec3f>(j, i)[2]) + center_y;*/
 
-			int x1 = ((-0.2 * image3d.at<Vec3f>(j, i)[0]));
-			int y1 = int(-0.2 * image3d.at<Vec3f>(j, i)[2]);
+			int x1 = ((-5 * image3d.at<Vec3f>(j, i)[0]));
+			int y1 = int(-5 * image3d.at<Vec3f>(j, i)[2]);
 
 			//int x2 = x1/cos(atan(y1/x1))*cos(atan(y1/x1)+azimuth-0.5)+position.x+center_x;
 			//int y2 = y1 / sin(atan(y1 / x1))*sin(atan(y1 / x1) + azimuth-0.5) +position.y+center_y;
