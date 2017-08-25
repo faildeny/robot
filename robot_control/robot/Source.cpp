da0e3895d3609f5b64b26182f7ad2d24e871d3c6
grzes_laptop <grzegorzskorupko@gmail.com>
2017-08-25 15:54:21 +0200

    fix

--- robot_control/robot/Source.cpp
+++ robot_control/robot/Source.cpp
@@ -475,7 +475,7 @@ while (true) {
 
 	high_resolution_clock::time_point time9 = high_resolution_clock::now();
 	auto duration2 = duration_cast<microseconds>(time9 - time3).count();
-	cout << "grab and remap and resize threaded: " << (double)duration2 / 1000 << " ms" << endl;
+	cout << "grab and remap and resize threaded: " << (double)duration9 / 1000 << " ms" << endl;
 
 	//thread visual_odometry(parallelOdometry, frame,vis_odo);
 	if(i==14) vis_odo.initOdometry(frame);
