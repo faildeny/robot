fd160c2285ddaa6b4a3a0878cd80f59947d0f3be
grzes_laptop <grzegorzskorupko@gmail.com>
2017-08-25 11:28:53 +0200

    counting fix

--- robot_control/robot/Source.cpp
+++ robot_control/robot/Source.cpp
@@ -413,8 +413,8 @@ while (true) {
 	cap2.retrieve(frame2);
 	
 	high_resolution_clock::time_point time2 = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(time2 - time1).count();
-	cout << "grab and retrieve: " << (double)duration / 1000 << " ms" << endl;
+	auto duration1 = duration_cast<microseconds>(time2 - time1).count();
+	cout << "grab and retrieve: " << (double)duration1 / 1000 << " ms" << endl;
 	//Visual odometry
 	
 	frame_detect = frame;
@@ -428,8 +428,8 @@ while (true) {
 	resize(frame2, frame2, Size(), 0.2, 0.2, INTER_AREA);
 	
 	high_resolution_clock::time_point time3 = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(time3 - time2).count();
-	cout << "remap and resize: " << (double)duration / 1000 << " ms" << endl;
+	auto duration2 = duration_cast<microseconds>(time3 - time2).count();
+	cout << "remap and resize: " << (double)duration2 / 1000 << " ms" << endl;
 
 	//thread visual_odometry(parallelOdometry, frame,vis_odo);
 	if(i==14) vis_odo.initOdometry(frame);
@@ -464,8 +464,8 @@ while (true) {
 	//imwrite("depth_full.bmp", disp);
 
 	high_resolution_clock::time_point time4 = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(time4 - time3).count();
-	cout << "cropping and BGR to GRAY: " << (double)duration / 1000 << " ms" << endl;
+	auto duration3 = duration_cast<microseconds>(time4 - time3).count();
+	cout << "cropping and BGR to GRAY: " << (double)duration3 / 1000 << " ms" << endl;
 	stereo.match(scan_line1, scan_line2, disp);
 
 	//disp.convertTo(disp8, CV_8U, 255 / (stereo.numberOfDisparities*16.));
@@ -482,8 +482,8 @@ while (true) {
 	turn= avoidDirection(disp);
 
 	high_resolution_clock::time_point time5 = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(time5 - time4).count();
-	cout << "matching and depthmap processing: " << (double)duration / 1000 << " ms" << endl;
+	auto duration4 = duration_cast<microseconds>(time5 - time4).count();
+	cout << "matching and depthmap processing: " << (double)duration4 / 1000 << " ms" << endl;
 //showing interface on the disparity image
 
 	/*applyColorMap(preview, preview, COLORMAP_JET);
@@ -538,8 +538,8 @@ while (true) {
 	//if (i%10 == 0) stream.send(posx, posy);
 
 	high_resolution_clock::time_point time6 = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(time6 - time5).count();
-	cout << "robot functions: " << (double)duration / 1000 << " ms" << endl;
+	auto duration5 = duration_cast<microseconds>(time6 - time5).count();
+	cout << "robot functions: " << (double)duration5 / 1000 << " ms" << endl;
 
 	auto duration = duration_cast<microseconds>(time6 - time1).count();
 	cout << "TOTAL TIME: " << (double)duration / 1000 << " ms" << endl;
