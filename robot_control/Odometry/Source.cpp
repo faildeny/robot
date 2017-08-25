1e9b3347ff7a51418b4f8b0d4b44e13669688301
grzes_laptop <grzegorzskorupko@gmail.com>
2017-08-25 11:03:30 +0200

    added chrono

--- robot_control/Odometry/Source.cpp
+++ robot_control/Odometry/Source.cpp
@@ -2,6 +2,10 @@
 
 #include <iostream>
 #include <stdio.h>
+
+#include <chrono>
+
+using namespace std::chrono;
 //#include "initial-state.h"
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -26,12 +30,17 @@ int main() {
 
 	while (true) {
 
-
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
 		cap.grab();
 		cap.retrieve(image);
 		//vis_odo.update(image);
 		imshow("frame", image);
 		waitKey(1);
+		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+		auto duration = duration_cast<microseconds>(t2 - t1).count();
+		cout << duration << endl;
+		cout << "elapsed time: " << (double)duration / 1000 << " ms" < , endl;
 	}
 	return 0;
 }
