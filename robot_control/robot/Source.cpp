18e168a3f081a4ba96077465aa8dd717ff57802f
grzes_laptop <grzegorzskorupko@gmail.com>
2017-09-04 11:29:20 +0200

    commented out old odometry

--- robot_control/robot/Source.cpp
+++ robot_control/robot/Source.cpp
@@ -80,132 +80,132 @@ bool detectAndDisplay(Mat frame, Rect &target)
 
 
 // Odometry module
-Size map_size(600, 600);
-Mat background= Mat::zeros(map_size.width, map_size.height, CV_8UC3);
-Mat robot_shape = Mat::zeros(map_size.width, map_size.height, CV_8UC3);
-Mat map = Mat::zeros(map_size.width, map_size.height, CV_8UC3);
-Point2d position(0, 0);
-int enc_left = 0;
-int enc_right = 0;
-int enc_left_old = 0;
-int enc_right_old = 0;
-int enc_diff_left = 0;
-int enc_diff_right = 0;
-int enc_l_dir = 1;
-int enc_r_dir = 1;
+//Size map_size(600, 600);
+//Mat background= Mat::zeros(map_size.width, map_size.height, CV_8UC3);
+//Mat robot_shape = Mat::zeros(map_size.width, map_size.height, CV_8UC3);
+//Mat map = Mat::zeros(map_size.width, map_size.height, CV_8UC3);
+//Point2d position(0, 0);
+//int enc_left = 0;
+//int enc_right = 0;
+//int enc_left_old = 0;
+//int enc_right_old = 0;
+//int enc_diff_left = 0;
+//int enc_diff_right = 0;
+//int enc_l_dir = 1;
+//int enc_r_dir = 1;
 
-double azimuth = 0;
-//old angle/encoder step ratio: 5.5
-double angleDeg = 5.325444;
-double angle_step = angleDeg*3.14159265 / 180;
-double move_step = 0.5;
-int view_dist=60;
-int view_angleDeg = 30;
-double view_angle=view_angleDeg*3.14159265 / 180;
-int view_res = 3;
-int center_x = 300;
-int center_y = 350;
+//double azimuth = 0;
+////old angle/encoder step ratio: 5.5
+//double angleDeg = 5.325444;
+//double angle_step = angleDeg*3.14159265 / 180;
+//double move_step = 0.5;
+//int view_dist=60;
+//int view_angleDeg = 30;
+//double view_angle=view_angleDeg*3.14159265 / 180;
+//int view_res = 3;
+//int center_x = 300;
+//int center_y = 350;
 
 
-void decodeEncoders() {
-	cout << "enc_left: " << enc_left << " enc_right: " << enc_right << endl;
-	enc_diff_left = (enc_l_dir < 0) ? -enc_left + enc_left_old : enc_left - enc_left_old;
-	enc_diff_right = (enc_r_dir < 0) ? -enc_right + enc_right_old : enc_right - enc_right_old;
-	enc_left_old = enc_left;
-	enc_right_old = enc_right;
-}
-
-void updateCoordinates(int left,int right) {
-	azimuth += (left-right)*angle_step;
-	position.x += ((left+right)*move_step)*sin(azimuth);
-	position.y += -((left+right)*move_step)*cos(azimuth);
-	cout << "left: " << left << " right: " << right << " azimuth: " << azimuth<<endl;
-
-	posx = posx_base + position.y*0.00000005;
-	posy = posy_base - position.x*0.00000005;
-
-}
-
-void drawRobot(Mat& image, Point centerPoint, Size rectangleSize, double rotationDegrees) {
-	image = Mat::zeros(image.cols,image.rows,image.type());
-	Scalar color = cv::Scalar(100,255,0); 
-    // Create the rotated rectangle
-	RotatedRect rotatedRectangle(centerPoint, rectangleSize, rotationDegrees);
-	// We take the edges that OpenCV calculated for us
-	Point2f vertices2f[4];
-	rotatedRectangle.points(vertices2f);
-	// Convert them so we can use them in a fillConvexPoly
-	Point vertices[4];
-	for (int i = 0; i < 4; ++i) {
-		vertices[i] = vertices2f[i];
-	}
-	// Now we can fill the rotated rectangle with our specified color
-	cv::fillConvexPoly(image,
-		vertices,
-		4,
-		color);
-}
-
-void drawCurrentArea(Mat& background, Point center, double azimuth) {
-		Scalar color = Scalar(70, 70, 100);
-		
-		Point vertices[4];
-		for (int i = -1; i < 2; i++) {
-			vertices[i+1].x = center.x + view_dist*sin(azimuth) + cos(azimuth)*sin(view_angle*i)*view_dist;
-			vertices[i+1].y = center.y - view_dist*cos(azimuth) + sin(azimuth)*sin(view_angle*i)*view_dist;
-		}
-		vertices[3] = center;
-
-		fillConvexPoly(background, vertices, 4, color);
-}
-
-void updateMap(Point2d position)
-{
-	//map = Mat::zeros(map.cols, map.rows, map.type());
-	int x = position.x + center_x;
-	int y = position.y + center_y;
-	circle(map, Point(x, y), 1, CV_RGB(0, 0, 255), 2);
-	drawRobot(robot_shape, Point(x, y), Size(10, 15), azimuth * 180 / 3.14);
-	drawCurrentArea(background, Point(x, y), azimuth);
-}
-
-void markTarget() {
-	int x1 = 1;
-	int y1 = -20;
-	int x2 = x1 / cos(atan(y1 / x1))*cos(atan(y1 / x1) + azimuth) + position.x + center_x;
-	int y2 = x1 / cos(atan(y1 / x1))*sin(atan(y1 / x1) + azimuth) + position.y + center_y;
-	putText(map, "object", Point(x2+3, y2), CV_FONT_HERSHEY_COMPLEX,0.25, Scalar(30, 255, 30), 0.5, LINE_4, 0);
-	circle(map, Point(x2, y2), 2, CV_RGB(30, 255, 30), 0.5);
-}
-
-Mat image3d;
-Mat scan_line1, scan_line2;
-
-void map3d(Mat &map, Mat image3d) {
-	int j = 120;
-	//map = Mat::zeros(600, 1280, CV_8UC3);
-	//Rect robot_rect(center_x - 10, center_y - 10, 20, 30);
-	//rectangle(map, robot_rect, Scalar(30, 255, 60), 2);
-	for (int j = 50; j < 51; j++) {
-		for (int i = 20; i < image3d.cols-20; i++) {
-
-			/*int x = int(5 * image3d.at<Vec3f>(j, i)[0]) + center_x);
-			int y = int(-5 * image3d.at<Vec3f>(j, i)[2]) + center_y;*/
-
-			int x1 = ((3 * image3d.at<Vec3f>(j, i)[0]));
-			int y1 = (-3 * image3d.at<Vec3f>(j, i)[2]);
-			//cout << "zaraz dodam " <<y1<<" "<<x1<< endl;
-			if (x1!= 0.0&& cos(atan(y1 / x1))!= 0.0) {
-				int x2 = x1 / cos(atan(y1 / x1))*cos(atan(y1 / x1) + azimuth) + position.x + center_x;
-				int y2 = x1 / cos(atan(y1 / x1))*sin(atan(y1 / x1) + azimuth) + position.y + center_y;
-				//int x2 = x1 + position.x + center_x;
-				//int y2 = y1 + position.y + center_y;
-				circle(map, Point(x2, y2), 0.5, CV_RGB(255, 0, 0), 0.5);
-			}
-			//cout << "dodalem" << endl;
-		}
-	}
-}
+//void decodeEncoders() {
+//	cout << "enc_left: " << enc_left << " enc_right: " << enc_right << endl;
+//	enc_diff_left = (enc_l_dir < 0) ? -enc_left + enc_left_old : enc_left - enc_left_old;
+//	enc_diff_right = (enc_r_dir < 0) ? -enc_right + enc_right_old : enc_right - enc_right_old;
+//	enc_left_old = enc_left;
+//	enc_right_old = enc_right;
+//}
+//
+//void updateCoordinates(int left,int right) {
+//	azimuth += (left-right)*angle_step;
+//	position.x += ((left+right)*move_step)*sin(azimuth);
+//	position.y += -((left+right)*move_step)*cos(azimuth);
+//	cout << "left: " << left << " right: " << right << " azimuth: " << azimuth<<endl;
+//
+//	posx = posx_base + position.y*0.00000005;
+//	posy = posy_base - position.x*0.00000005;
+//
+//}
+//
+//void drawRobot(Mat& image, Point centerPoint, Size rectangleSize, double rotationDegrees) {
+//	image = Mat::zeros(image.cols,image.rows,image.type());
+//	Scalar color = cv::Scalar(100,255,0); 
+//    // Create the rotated rectangle
+//	RotatedRect rotatedRectangle(centerPoint, rectangleSize, rotationDegrees);
+//	// We take the edges that OpenCV calculated for us
+//	Point2f vertices2f[4];
+//	rotatedRectangle.points(vertices2f);
+//	// Convert them so we can use them in a fillConvexPoly
+//	Point vertices[4];
+//	for (int i = 0; i < 4; ++i) {
+//		vertices[i] = vertices2f[i];
+//	}
+//	// Now we can fill the rotated rectangle with our specified color
+//	cv::fillConvexPoly(image,
+//		vertices,
+//		4,
+//		color);
+//}
+//
+//void drawCurrentArea(Mat& background, Point center, double azimuth) {
+//		Scalar color = Scalar(70, 70, 100);
+//		
+//		Point vertices[4];
+//		for (int i = -1; i < 2; i++) {
+//			vertices[i+1].x = center.x + view_dist*sin(azimuth) + cos(azimuth)*sin(view_angle*i)*view_dist;
+//			vertices[i+1].y = center.y - view_dist*cos(azimuth) + sin(azimuth)*sin(view_angle*i)*view_dist;
+//		}
+//		vertices[3] = center;
+//
+//		fillConvexPoly(background, vertices, 4, color);
+//}
+//
+//void updateMap(Point2d position)
+//{
+//	//map = Mat::zeros(map.cols, map.rows, map.type());
+//	int x = position.x + center_x;
+//	int y = position.y + center_y;
+//	circle(map, Point(x, y), 1, CV_RGB(0, 0, 255), 2);
+//	drawRobot(robot_shape, Point(x, y), Size(10, 15), azimuth * 180 / 3.14);
+//	drawCurrentArea(background, Point(x, y), azimuth);
+//}
+//
+//void markTarget() {
+//	int x1 = 1;
+//	int y1 = -20;
+//	int x2 = x1 / cos(atan(y1 / x1))*cos(atan(y1 / x1) + azimuth) + position.x + center_x;
+//	int y2 = x1 / cos(atan(y1 / x1))*sin(atan(y1 / x1) + azimuth) + position.y + center_y;
+//	putText(map, "object", Point(x2+3, y2), CV_FONT_HERSHEY_COMPLEX,0.25, Scalar(30, 255, 30), 0.5, LINE_4, 0);
+//	circle(map, Point(x2, y2), 2, CV_RGB(30, 255, 30), 0.5);
+//}
+//
+//Mat image3d;
+//Mat scan_line1, scan_line2;
+//
+//void map3d(Mat &map, Mat image3d) {
+//	int j = 120;
+//	//map = Mat::zeros(600, 1280, CV_8UC3);
+//	//Rect robot_rect(center_x - 10, center_y - 10, 20, 30);
+//	//rectangle(map, robot_rect, Scalar(30, 255, 60), 2);
+//	for (int j = 50; j < 51; j++) {
+//		for (int i = 20; i < image3d.cols-20; i++) {
+//
+//			/*int x = int(5 * image3d.at<Vec3f>(j, i)[0]) + center_x);
+//			int y = int(-5 * image3d.at<Vec3f>(j, i)[2]) + center_y;*/
+//
+//			int x1 = ((3 * image3d.at<Vec3f>(j, i)[0]));
+//			int y1 = (-3 * image3d.at<Vec3f>(j, i)[2]);
+//			//cout << "zaraz dodam " <<y1<<" "<<x1<< endl;
+//			if (x1!= 0.0&& cos(atan(y1 / x1))!= 0.0) {
+//				int x2 = x1 / cos(atan(y1 / x1))*cos(atan(y1 / x1) + azimuth) + position.x + center_x;
+//				int y2 = x1 / cos(atan(y1 / x1))*sin(atan(y1 / x1) + azimuth) + position.y + center_y;
+//				//int x2 = x1 + position.x + center_x;
+//				//int y2 = y1 + position.y + center_y;
+//				circle(map, Point(x2, y2), 0.5, CV_RGB(255, 0, 0), 0.5);
+//			}
+//			//cout << "dodalem" << endl;
+//		}
+//	}
+//}
 // Reproject image to 3D
 void customReproject(const cv::Mat& disparity, const cv::Mat& Q, cv::Mat& out3D)
 {
